d18cfb7af9045d0ca630b000d340f64254d63f6c
galeree <galeguy@hotmail.com>
2016-05-31 14:56:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Initial Setting
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var app = require('express').createServer(),
@@ -7,23 +8,22 @@ var app = require('express').createServer(),
 	cronjob = require('cron').CronJob,
 	readline = require('readline');
 
+// Event emitter
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
-
-/*app.get('/', function(req, res) {
-  res.send('hello world');
-});*/
 
 app.listen(server_port, server_ip_address, function() {
 	console.log( "Listening on " + server_ip_address + ", server_port ");
 });
 
+// Add hour to date
 Date.prototype.addHours= function(h){
     this.setHours(this.getHours()+h);
     return this;
 }
 
-/*var keyword = [];
+// Get keyword for search from keyword.txt file
+var keyword = [];
 var rd = readline.createInterface({
 	    input: fs.createReadStream('keyword.txt'),
 	    output: process.stdout,
@@ -47,19 +47,20 @@ rd.on('close', function(err) {
 	eventEmitter.emit('fileread');
 });
 
+// Query message from twitter with specify keyword
 eventEmitter.on('fileread', function() {
 	new cronjob('0 0 6 * * 1-5', function() {
 		var date = new Date().addHours(12).toDateString().split(" ").join("_");
 		var file = process.env.OPENSHIFT_DATA_DIR +"/tweets_"+ date +".json";
 		var count = 0;
-
+		// Specify twiiter api key here
 		var twit = new twitter({
-			consumer_key: 'oCJMD8RsD2tLXZmlvubD9pIfg',
-			consumer_secret: 'da3c724ozRUsZsg2MVrGNOBCisJ8ssSKOpYSuKxvb3gu8LGPi7',
-			access_token_key: '1872179137-bORd018hRmxqwnU3w04IsdSO3ZD05vRbxY6F7D0',
-			access_token_secret: 'LGhx0s0FmhvVbBwd4g65uXfSayvxlYHOdXFjWjVeMQE5U'
+			consumer_key: 'xxxxxxxx',
+			consumer_secret: 'xxxxxxxx',
+			access_token_key: 'xxxxxxxx',
+			access_token_secret: 'xxxxxxxx'
 		});
-
+		// Use Twitter stream API to get message data
 		twit.stream('statuses/filter', { track: [ keyword ] ,language : "th"}, function(stream) {
 			var end = 0;
 			var start_time = new Date();
@@ -83,4 +84,4 @@ eventEmitter.on('fileread', function() {
 			setTimeout(stream.destroy, 64800000);
 		});
 	}, null, true, 'Asia/Bangkok');
-});*/
+});
